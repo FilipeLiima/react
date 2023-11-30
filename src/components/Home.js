8edd1804ed174298edfff81d9d5b396443459afc
Filipe Lima <131200594+FilipeLiima@users.noreply.github.com>
2023-11-30 11:34:37 -0300

    react update

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
         <nav className="navegation">
           <ul>
             <li>
-              <a href="index.html">HOME</a>
+              <a href="https://filipeliima.github.io/react/">HOME</a>
             </li>
             <li>
               <a href="#portfolio-section">PROJETOS</a>
