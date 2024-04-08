4711689aea703292682e479eafdd856abdbf14ad
Filipe Lima <131200594+FilipeLiima@users.noreply.github.com>
2024-04-08 12:20:03 -0300

    up

--- vite.config.ts
+++ vite.config.ts
@@ -9,14 +9,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: "build",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "./src/App.tsx"),
-      },
-    },
-  },
+
   base: "/react/",
 });
