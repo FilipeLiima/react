58804ef9031d75fee29d926a38b3c13fbd972466
Filipe Lima <131200594+FilipeLiima@users.noreply.github.com>
2024-04-08 14:55:45 -0300

    up

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./global.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react">
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
