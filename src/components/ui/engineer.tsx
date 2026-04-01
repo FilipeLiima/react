2534e685af5dee3049d12e8cbf1f1f69a7410a1f
Filipe Lima <felipesantana2222@hotmail.com>
2026-04-01 19:17:26 -0300

    up

--- src/components/ui/engineer.tsx
+++ src/components/ui/engineer.tsx
@@ -3,10 +3,7 @@ import Eng from "../../assets/eng_2.svg";
 export function Engineer() {
   return (
     <div className="bg-black text-white flex justify-center items-center">
-      <img 
-        src={Eng} 
-        alt="eng" 
-      />
+      <img src={Eng} alt="eng" className="w-4/5 h-auto" />
     </div>
   );
 }
